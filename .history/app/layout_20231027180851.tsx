643efd11fa0e83f5264c309ae81edb5f6d8311a7
Sergey Olin <sergeyolin@ranked-choice.com>
2023-10-30 19:01:37 +0300

    First commit

---
+++ .history/app/layout_20231027180851.tsx
@@ -0,0 +1,15 @@
+import './ui/global.css'
+
+import {inter} from './'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
